9e83afa8ad3ec23b22605b414f0faf1be96b6656
Shuhei Kadowaki <aviatesk@gmail.com>
2021-10-27 02:17:26 +0900

    optimizer: inline union-split, abstract callsite
    Currently we can inline (or `:invoke`) abstract callsite only when there
    is a single dispatch candidate, and inlining and static-dispatch will
    fail when the callsite is union-split (in other word, our inlinear can
    handle union-split callsite only when all the dispatch candidates are concrete):
    ```julia
    julia> @inline isType(@nospecialize t) = isa(t, DataType) && t.name === Type.body.name
    isType (generic function with 1 method)
    
    julia> code_typed((Any,)) do x # very abstract, successful inlining
               isType(x)
           end |> only
    CodeInfo(
    1 ─ %1 = (x isa Main.DataType)::Bool
    └──      goto #3 if not %1
    2 ─ %3 = π (x, DataType)
    │   %4 = Base.getfield(%3, :name)::Core.TypeName
    │   %5 = Base.getfield(Type{T}, :name)::Core.TypeName
    │   %6 = (%4 === %5)::Bool
    └──      goto #4
    3 ─      goto #4
    4 ┄ %9 = φ (#2 => %6, #3 => false)::Bool
    └──      return %9
    ) => Bool
    
    julia> code_typed((Union{Type,Nothing},)) do x # union-split, unsuccessful inlining
               isType(x)
           end |> only
    CodeInfo(
    1 ─ %1 = (isa)(x, Nothing)::Bool
    └──      goto #3 if not %1
    2 ─      goto #4
    3 ─ %4 = Main.isType(x)::Bool
    └──      goto #4
    4 ┄ %6 = φ (#2 => false, #3 => %4)::Bool
    └──      return %6
    ) => Bool
    ```
    Note that this is a limitation of inlinear and so user-provided hints
    (like callsite inlining) doesn't help here.
    
    This commit enables inlining and static dispatch for union-split,
    abstract callsite. The key insight is that we can simulate the dispatch
    semantics by generating `isa` checks by sorting dispatch candidates by
    their signature specialities and processing them in that order.
    ```julia
    julia> code_typed((Union{Type,Nothing},)) do x # union-split, unsuccessful inlining
                      isType(x)
                  end |> only
    CodeInfo(
    1 ─ %1  = (isa)(x, Nothing)::Bool
    └──       goto #3 if not %1
    2 ─       goto #9
    3 ─ %4  = (isa)(x, Type)::Bool
    └──       goto #8 if not %4
    4 ─ %6  = π (x, Type)
    │   %7  = (%6 isa Main.DataType)::Bool
    └──       goto #6 if not %7
    5 ─ %9  = π (%6, DataType)
    │   %10 = Base.getfield(%9, :name)::Core.TypeName
    │   %11 = Base.getfield(Type{T}, :name)::Core.TypeName
    │   %12 = (%10 === %11)::Bool
    └──       goto #7
    6 ─       goto #7
    7 ┄ %15 = φ (#5 => %12, #6 => false)::Bool
    └──       goto #9
    8 ─       Core.throw(ErrorException("fatal error in type inference (type bound)"))::Union{}
    └──       unreachable
    9 ┄ %19 = φ (#2 => false, #7 => %15)::Bool
    └──       return %19
    ) => Bool
    ```
    
    Inlined and/or statically-resolved union-split abstract callsite will
    hopefully improve the performance in general, and also, it seems to help
    us avoid some excessive specializations by statically-resolving `@nospecialize`d
    callsites. Especially, the # of precompiled statements is reduced to `1385`
    (this commit) from `1517` ([`master`](021a6b5ec99639a38a639b0df993259d58cdd57b)),
    probably because this commit eliminates some specializations in `Core.Compiler` code.
    And also as a side effect, the implementation of inlinear gets a bit
    simplified since now we no longer need the special casing for abstract callsites.
    
    Obviously the possible drawback would be excessive code size. We need to
    carefully inspect the impacts on compile-time and runtime.

--- base/compiler/ssair/inlining.jl
+++ base/compiler/ssair/inlining.jl
@@ -60,7 +60,8 @@ struct InliningCase
     sig  # Type
     item # Union{InliningTodo, MethodInstance, ConstantCase}
     function InliningCase(@nospecialize(sig), @nospecialize(item))
-        @assert isa(item, Union{InliningTodo, InvokeCase, ConstantCase}) "invalid inlining item"
+        @assert item !== nothing
+        @assert isa(item, Union{InliningTodo, InvokeCase, ConstantCase, Nothing}) "invalid inlining item"
         return new(sig, item)
     end
 end
@@ -472,6 +473,10 @@ function ir_inline_unionsplit!(compact::IncrementalCompact, idx::Int,
     pn = PhiNode()
     local bb = compact.active_result_bb
     @assert length(bbs) >= length(cases)
+    # we may deal with union-split, abstract callsites here,
+    # and so we sort all inlining candidates by signature speciality
+    # so that the generated `isa` checks simulates the dispatch semantics
+    sort!(cases; lt=morespecific, by=case::InliningCase->case.sig)
     for i in 1:length(cases)
         ithcase = cases[i]
         mtype = ithcase.sig::DataType # checked within `handle_cases!`
@@ -515,6 +520,9 @@ function ir_inline_unionsplit!(compact::IncrementalCompact, idx::Int,
         end
         if isa(case, InliningTodo)
             val = ir_inline_item!(compact, idx, argexprs′, linetable, case, boundscheck, todo_bbs)
+        # elseif case === nothing
+        #     val = insert_node_here!(compact,
+        #         NewInstruction(Expr(:call, argexprs′...), typ, line))
         elseif isa(case, InvokeCase)
             effect_free = is_removable_if_unused(case.effects)
             val = insert_node_here!(compact,
@@ -1175,12 +1183,8 @@ function analyze_single_call!(
     sig::Signature, state::InliningState, todo::Vector{Pair{Int, Any}})
     argtypes = sig.argtypes
     cases = InliningCase[]
-    local only_method = nothing  # keep track of whether there is one matching method
-    local meth::MethodLookupResult
+    local fully_covered = true
     local handled_all_cases = true
-    local any_covers_full = false
-    local revisit_idx = nothing
-
     for i in 1:length(infos)
         meth = infos[i].results
         if meth.ambig
@@ -1191,66 +1195,23 @@ function analyze_single_call!(
             # No applicable methods; try next union split
             handled_all_cases = false
             continue
-        else
-            if length(meth) == 1 && only_method !== false
-                if only_method === nothing
-                    only_method = meth[1].method
-                elseif only_method !== meth[1].method
-                    only_method = false
-                end
-            else
-                only_method = false
-            end
         end
-        for (j, match) in enumerate(meth)
-            any_covers_full |= match.fully_covers
-            if !isdispatchtuple(match.spec_types)
-                if !match.fully_covers
-                    handled_all_cases = false
-                    continue
-                end
-                if revisit_idx === nothing
-                    revisit_idx = (i, j)
-                else
-                    handled_all_cases = false
-                    revisit_idx = nothing
-                end
-            else
-                handled_all_cases &= handle_match!(match, argtypes, flag, state, cases)
-            end
+        for match in meth
+            handled_all_cases &= handle_match!(match, argtypes, flag, state, cases, true)
+            fully_covered &= match.fully_covers
         end
     end
 
-    atype = argtypes_to_type(argtypes)
-    if handled_all_cases && revisit_idx !== nothing
-        # If there's only one case that's not a dispatchtuple, we can
-        # still unionsplit by visiting all the other cases first.
-        # This is useful for code like:
-        #   foo(x::Int) = 1
-        #   foo(@nospecialize(x::Any)) = 2
-        # where we where only a small number of specific dispatchable
-        # cases are split off from an ::Any typed fallback.
-        (i, j) = revisit_idx
-        match = infos[i].results[j]
-        handled_all_cases &= handle_match!(match, argtypes, flag, state, cases, true)
-    elseif length(cases) == 0 && only_method isa Method
-        # if the signature is fully covered and there is only one applicable method,
-        # we can try to inline it even if the signature is not a dispatch tuple.
-        # -- But don't try it if we already tried to handle the match in the revisit_idx
-        # case, because that'll (necessarily) be the same method.
-        if length(infos) > 1
-            (metharg, methsp) = ccall(:jl_type_intersection_with_env, Any, (Any, Any),
-                atype, only_method.sig)::SimpleVector
-            match = MethodMatch(metharg, methsp::SimpleVector, only_method, true)
-        else
-            @assert length(meth) == 1
-            match = meth[1]
-        end
-        handle_match!(match, argtypes, flag, state, cases, true) || return nothing
-        any_covers_full = handled_all_cases = match.fully_covers
+    if all(case::InliningCase->case.item===nothing, cases)
+        # all split is going to be dynamic dispatch, just bail out
+        return nothing
+    elseif !handled_all_cases
+        # if we've not seen all candidates, union split is valid only for dispatch tuples
+        filter!(case::InliningCase->isdispatchtuple(case.sig), cases)
     end
 
-    handle_cases!(ir, idx, stmt, atype, cases, any_covers_full && handled_all_cases, todo, state.params)
+    handle_cases!(ir, idx, stmt, argtypes_to_type(argtypes), cases,
+        handled_all_cases & fully_covered, todo, state.params)
 end
 
 # similar to `analyze_single_call!`, but with constant results
@@ -1261,8 +1222,8 @@ function handle_const_call!(
     (; call, results) = cinfo
     infos = isa(call, MethodMatchInfo) ? MethodMatchInfo[call] : call.matches
     cases = InliningCase[]
+    local fully_covered = true
     local handled_all_cases = true
-    local any_covers_full = false
     local j = 0
     for i in 1:length(infos)
         meth = infos[i].results
@@ -1278,32 +1239,29 @@ function handle_const_call!(
         for match in meth
             j += 1
             result = results[j]
-            any_covers_full |= match.fully_covers
+            fully_covered &= match.fully_covers
             if isa(result, ConstResult)
                 case = const_result_item(result, state)
                 push!(cases, InliningCase(result.mi.specTypes, case))
             elseif isa(result, InferenceResult)
-                handled_all_cases &= handle_inf_result!(result, argtypes, flag, state, cases)
+                handled_all_cases &= handle_inf_result!(result, argtypes, flag, state, cases, true)
             else
                 @assert result === nothing
-                handled_all_cases &= handle_match!(match, argtypes, flag, state, cases)
+                handled_all_cases &= handle_match!(match, argtypes, flag, state, cases, true)
             end
         end
     end
 
-    # if the signature is fully covered and there is only one applicable method,
-    # we can try to inline it even if the signature is not a dispatch tuple
-    atype = argtypes_to_type(argtypes)
-    if length(cases) == 0
-        length(results) == 1 || return nothing
-        result = results[1]
-        isa(result, InferenceResult) || return nothing
-        handle_inf_result!(result, argtypes, flag, state, cases, true) || return nothing
-        spec_types = cases[1].sig
-        any_covers_full = handled_all_cases = atype <: spec_types
+    if all(case::InliningCase->case.item===nothing, cases)
+        # all split is going to be dynamic dispatch, just bail out
+        return nothing
+    elseif !handled_all_cases
+        # if we've not seen all candidates, union split is valid only for dispatch tuples
+        filter!(case::InliningCase->isdispatchtuple(case.sig), cases)
     end
 
-    handle_cases!(ir, idx, stmt, atype, cases, any_covers_full && handled_all_cases, todo, state.params)
+    handle_cases!(ir, idx, stmt, argtypes_to_type(argtypes), cases,
+        handled_all_cases & fully_covered, todo, state.params)
 end
 
 function handle_match!(
@@ -1313,7 +1271,6 @@ function handle_match!(
     allow_abstract || isdispatchtuple(spec_types) || return false
     item = analyze_method!(match, argtypes, flag, state)
     item === nothing && return false
-    _any(case->case.sig === spec_types, cases) && return true
     push!(cases, InliningCase(spec_types, item))
     return true
 end
@@ -1445,7 +1402,8 @@ function assemble_inline_todo!(ir::IRCode, state::InliningState)
 
         analyze_single_call!(ir, idx, stmt, infos, flag, sig, state, todo)
     end
-    todo
+
+    return todo
 end
 
 function linear_inline_eligible(ir::IRCode)
